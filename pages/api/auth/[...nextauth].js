ff58144642173d62566f4829709749d4b43067df
Steven <s.gonza.gills@gmail.com>
2023-01-11 22:57:33 -0500

    New edit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,6 +5,7 @@ import GoogleProvider from 'next-auth/providers/google'
 // import EmailProvider from 'next-auth/providers/email'
 
 export default NextAuth({
+    secret: process.env.SECRET,
   providers: [
     // OAuth authentication providers...
     // AppleProvider({
