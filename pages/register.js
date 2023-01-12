46371eb83911efe600f2729b732a512af345549b
Steven <s.gonza.gills@gmail.com>
2023-01-12 15:38:02 -0500

    New edit

---
+++ pages/register.js
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+// import styles from '../styles/register.module.css'
+
+const register = () => {
+    const [ email, setEmail ] = useState('')
+    const [ password, setPassword] = useState('')
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default register
