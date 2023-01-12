7efa83a5bbf5f8d09d8dd23d0f0834e3b349e08f
Steven <s.gonza.gills@gmail.com>
2023-01-11 22:54:48 -0500

    New edit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,7 +15,6 @@ export default NextAuth({
     //   clientId: process.env.FACEBOOK_ID,
     //   clientSecret: process.env.FACEBOOK_SECRET
     // }),
-    secret: process.env.SECRET
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
