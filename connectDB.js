962ba601b96149f35330e2a1ea9ecacbb6dcd651
Steven <s.gonza.gills@gmail.com>
2023-01-13 16:10:33 -0500

    New edit

--- connectDB.js
+++ connectDB.js
@@ -4,5 +4,10 @@ const connectDB = async () => {
     if (mongoose.connection.readyState >= 1) {
         return
     }
-    mongoose.connect
+    mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then((con) => console.log('Mongo connected.'))
 }
