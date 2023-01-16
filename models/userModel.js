bd958a554e5bfc26c3a314bec009aed10d47d504
Steven <s.gonza.gills@gmail.com>
2023-01-15 23:11:16 -0500

    New edit

---
+++ models/userModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    userName: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        unique: false,
+        required: true,
+    },
+    // profilePicture: {
+    //  type: String,
+    // }
+  })
+
+  export default mongoose.models.User || mongoose.model('User', userSchema)
