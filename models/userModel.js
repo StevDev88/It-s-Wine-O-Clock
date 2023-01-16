8a4ae9a5e469c2e86dc66ffa0633169ce49783b9
Steven <s.gonza.gills@gmail.com>
2023-01-15 23:19:50 -0500

    New edit

--- models/userModel.js
+++ models/userModel.js
@@ -21,6 +21,6 @@ const userSchema = mongoose.Schema({
     // }
   })
 
-  const User = mongoose.model('User', userSchema)
+  // const User = mongoose.model('User', userSchema)
 
   export default mongoose.models.User || mongoose.model('User', userSchema)
