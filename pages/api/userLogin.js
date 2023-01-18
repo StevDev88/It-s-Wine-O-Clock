59b3520ef36acdf8a00e12ff9795572f10db76ca
Steven <s.gonza.gills@gmail.com>
2023-01-18 00:18:12 -0500

    New edit

---
+++ pages/api/userLogin.js
@@ -0,0 +1,32 @@
+import connectDB from "../../connectDB"
+import User from '../../models/userModel'
+import bcrypt from "bcryptjs"
+
+connectDB()
+
+export default async (req, res) => {
+
+    console.log(req.method)
+
+    // try {
+    //     if (req.method === 'POST') {
+    //         const { email, password } = req.body
+    //         const user = await User.findOne({email: email})
+
+    //         if (user) {
+    //             res.status(422).json({message: "User already exists."})
+    //         }
+
+    //         console.log(email, password)
+        
+    //         const hashedPassword = await bcrypt.hash(password, 12)
+    //         const newUser = await new User({
+    //             email: email,
+    //             password: hashedPassword
+    //         }).save()
+    //         res.status(200).json({ message: "Sign Up Success"})
+    //     }
+    // } catch (error) {
+    //     console.log(error)
+    // }
+}
