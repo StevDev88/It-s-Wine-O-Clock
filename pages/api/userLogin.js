1ed352f1a098791e7dfc511650dfc8616587cd0f
Steven <s.gonza.gills@gmail.com>
2023-01-18 00:32:46 -0500

    New edit

--- pages/api/userLogin.js
+++ pages/api/userLogin.js
@@ -1,6 +1,7 @@
 import connectDB from "../../connectDB"
 import User from '../../models/userModel'
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 connectDB()
 
@@ -24,6 +25,7 @@ export default async (req, res) => {
             } else {
                 const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {expiresIn: '7d',
                 })
+                res.status(201).json({ message: "Login success", user, token }) 
             }
         }
     } catch (error) {
