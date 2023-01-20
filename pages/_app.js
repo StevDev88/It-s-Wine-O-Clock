83201b5a85c02bfa11e5c8f418f5b8ad648822dc
Steven <s.gonza.gills@gmail.com>
2023-01-20 13:13:38 -0500

    New edit

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,42 @@
 import Layout from "@/components/Layout"
 import { SessionProvider } from "next-auth/react"
 
-export default function App({
-  Component, pageProps: { session, ...pageProps }
-}) {
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/theme';
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <SessionProvider session={session}>
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <SessionProvider session={session}>
         <Layout>
             <Component {...pageProps}/>
         </Layout>
     </SessionProvider>
-  )
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
