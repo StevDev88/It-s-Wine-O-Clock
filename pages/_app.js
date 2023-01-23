e9dfa71848ea2881fb9e8d7060d3885032e05f0f
Steven <s.gonza.gills@gmail.com>
2023-01-23 13:19:15 -0500

    New edit

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,8 @@ import { SessionProvider } from "next-auth/react"
 import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import theme from '../src/theme';
 
 export default function MyApp(props) {
