2f70e8364f8726534330ac77064a307d0eea6b44
Steven <s.gonza.gills@gmail.com>
2023-01-26 00:37:33 -0500

    New edit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -137,7 +137,25 @@ function ResponsiveAppBar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                {user && user.email}
+
+              <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            {user && user.email}
+          </Typography>
               </IconButton>
             </Tooltip>
             <Menu
