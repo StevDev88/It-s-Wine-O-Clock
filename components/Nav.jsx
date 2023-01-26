2d49dabe5abc5545ffb0deb6a8db8d0f82b7c3c5
Steven <s.gonza.gills@gmail.com>
2023-01-26 01:53:34 -0500

    New edit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -112,7 +112,7 @@ function ResponsiveAppBar() {
             variant="h5"
             noWrap
             component="a"
-            href=""
+            href="/"
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },
