a50fff36933adbd042dcd46f3b8f6f9bc8b790be
Steven <s.gonza.gills@gmail.com>
2023-01-27 17:57:48 -0500

    New edit

--- config/theme.js
+++ config/theme.js
@@ -1,13 +1,14 @@
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
+
 // Create a theme instance.
 const theme = createTheme({
 palette: {
    primary: {
-      main: '#556cd6',
+      main: '#800000',
    },
    secondary: {
-     main: '#19857b',
+     main: '#fff8dc',
    },
    error: {
    main: red.A400,
