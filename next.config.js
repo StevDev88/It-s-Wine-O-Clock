9b28ccd64c888c025b3d3e1000afff71b37a035f
Steven <s.gonza.gills@gmail.com>
2023-01-27 16:01:17 -0500

    New edit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+ 
+module.exports = {
+    compiler: {
+      emotion: boolean | {
+        // default is true. It will be disabled when build type is production.
+        sourceMap?: boolean,
+        // default is 'dev-only'.
+        autoLabel?: 'never' | 'dev-only' | 'always',
+        // default is '[local]'.
+        // Allowed values: `[local]` `[filename]` and `[dirname]`
+        // This option only works when autoLabel is set to 'dev-only' or 'always'.
+        // It allows you to define the format of the resulting label.
+        // The format is defined via string where variable parts are enclosed in square brackets [].
+        // For example labelFormat: "my-classname--[local]", where [local] will be replaced with the name of the variable the result is assigned to.
+        labelFormat?: string,
+        // default is undefined.
+        // This option allows you to tell the compiler what imports it should
+        // look at to determine what it should transform so if you re-export
+        // Emotion's exports, you can still use transforms.
+        importMap?: {
+          [packageName: string]: {
+            [exportName: string]: {
+              canonicalImport?: [string, string],
+              styledBaseImport?: [string, string],
+            }
+          }
+        },
+      },
+    },
+  }
