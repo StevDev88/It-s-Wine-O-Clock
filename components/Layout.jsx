469e18c8b44f036ef466d9694ba2b348b22d5551
Steven <s.gonza.gills@gmail.com>
2023-01-30 02:27:59 -0500

    New edit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,15 +2,17 @@ import { Container } from "@mui/material"
 import Header from "./Header"
 import Nav from "./Nav";
 
-// import styles from '../styles/Layout.module.css'
+import styles from '../styles/Layout.module.css'
 
 const Layout = ({ children }) => {
     return (
         <>
-        <Container>
-            <Header />
-            { children }
-        </Container>
+        <div className={styles.div}>
+            <Container>
+                <Header />
+                { children }
+            </Container>
+        </div>
         </>
     )
 }
