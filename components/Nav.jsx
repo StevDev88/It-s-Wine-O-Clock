19ffc38a67ee96f0b049d536cd2f3c623b2725c7
Steven <s.gonza.gills@gmail.com>
2023-01-30 18:12:29 -0500

    New edit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -140,7 +140,8 @@ function ResponsiveAppBar() {
                 component={NextLink}
                 href={page}
                 >
-                {page}</Link>
+                    {page}
+                </Link>
               </Button>
             ))}
           </Box>
