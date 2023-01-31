8de755ed266c8d495ce9b9d70a7249383fe4afa4
Steven <s.gonza.gills@gmail.com>
2023-01-31 02:53:42 -0500

    New edit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,25 +13,25 @@ export default NextAuth({
 
     secret: process.env.JWT_SECRET,
 
-  providers: [
-    // OAuth authentication providers...
+    providers: [
+        // OAuth authentication providers...
 
-    // AppleProvider({
-    //   clientId: process.env.APPLE_ID,
-    //   clientSecret: process.env.APPLE_SECRET
-    // }),
-    // FacebookProvider({
-    //   clientId: process.env.FACEBOOK_ID,
-    //   clientSecret: process.env.FACEBOOK_SECRET
-    // }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
-    }),
-    // Passwordless / email sign in
-    // EmailProvider({
-    //   server: process.env.MAIL_SERVER,
-    //   from: 'NextAuth.js <no-reply@example.com>'
-    // }),
-  ]
+        // AppleProvider({
+        //   clientId: process.env.APPLE_ID,
+        //   clientSecret: process.env.APPLE_SECRET
+        // }),
+        // FacebookProvider({
+        //   clientId: process.env.FACEBOOK_ID,
+        //   clientSecret: process.env.FACEBOOK_SECRET
+        // }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET
+        }),
+        // Passwordless / email sign in
+        // EmailProvider({
+        //   server: process.env.MAIL_SERVER,
+        //   from: 'NextAuth.js <no-reply@example.com>'
+        // }),
+    ]
 })
