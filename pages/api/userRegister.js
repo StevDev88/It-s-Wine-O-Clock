01570af329bb999f9fd22e02c84131b0b62db365
Steven <s.gonza.gills@gmail.com>
2023-01-31 02:54:29 -0500

    New edit

--- pages/api/userRegister.js
+++ pages/api/userRegister.js
@@ -9,20 +9,20 @@ export default async (req, res) => {
     try {
         if (req.method === 'POST') {
             const { email, password } = req.body
-            const user = await User.findOne({email: email})
+            const user = await User.findOne({ email: email })
 
             if (user) {
-                res.status(422).json({message: "User already exists."})
+                res.status(422).json({ message: "User already exists." })
             }
 
             console.log(email, password)
-        
+
             const hashedPassword = await bcrypt.hash(password, 12)
             const newUser = await new User({
                 email: email,
                 password: hashedPassword
             }).save()
-            res.status(200).json({ message: "Sign Up Success"})
+            res.status(200).json({ message: "Sign Up Success" })
         }
     } catch (error) {
         console.log(error)
