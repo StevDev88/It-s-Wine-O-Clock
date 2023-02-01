c9df57741dda58b66808d52ecf07bf3bd580b75c
Steven <s.gonza.gills@gmail.com>
2023-02-01 02:55:02 -0500

    New edit

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,16 @@
 import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Component() {
-  const { data: session } = useSession()
-  console.log(session)
-  if(session) {
+    const { data: session } = useSession()
+    console.log(session)
+    if (session) {
+        return <>
+            Signed in as {session.user.email} <br />
+            <button onClick={() => signOut()}>Sign out</button>
+        </>
+    }
     return <>
-      Signed in as {session.user.email} <br/>
-      <button onClick={() => signOut()}>Sign out</button>
+        Not signed in <br />
+        <button onClick={() => signIn('google')}>Sign in</button>
     </>
-  }
-  return <>
-    Not signed in <br/>
-    <button onClick={() => signIn('google')}>Sign in</button>
-  </>
 }
