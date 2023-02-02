fbe820c07f234fba96b07cf1c47d2c5bdf8a5db5
Steven <s.gonza.gills@gmail.com>
2023-02-02 02:56:36 -0500

    New edit

--- connectDB.js
+++ connectDB.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 
-
 const connectDB = async () => {
     if (mongoose.connection.readyState >= 1) {
         return
     }
     mongoose.connect(process.env.MONGODB_URI, {
     })
-    .then((con) => console.log('Mongo connected.'))
+        .then((con) => console.log('Mongo connected.'))
 }
 
 export default connectDB
