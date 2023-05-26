d6afbb60cf1b3da5758778ee9163e6ec12eb4e7f
Steven <s.gonza.gills@gmail.com>
2023-05-26 15:11:46 -0400

    New edit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-// import Head from 'next/head'
-// import Image from 'next/image'
-
-// import styles from '@/styles/Home.module.css'
 // import styles from '../styles/index.module.css'
 
 import { useSession, signIn, signOut } from "next-auth/react"
