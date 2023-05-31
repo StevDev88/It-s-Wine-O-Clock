5e8957a05167dd937de0c985f2896908be62b990
Steven <s.gonza.gills@gmail.com>
2023-05-31 01:10:21 -0400

    New edit

---
+++ pages/login.js
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import axios from "axios"
+
+// import styles from '../styles/login.module.css'
+
+const login = () => {
+    const [ email, setEmail ] = useState('')
+    const [ password, setPassword] = useState('')
+    const submitHandler = async (e) => {
+        e.preventDefault()
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json"
+            },
+        }
+
+        const { data } = await axios.post(
+            `/api/userLogin`,
+            {email, password},
+            config
+            )
+
+        console.log(data)
+
+    }
+
+    return (
+        <>
+        <form onSubmit={submitHandler}>
+        <h1>Log In</h1>        
+        <input value={email} onChange={(e)=>setEmail(e.target.value)} />
+        <input value={password} onChange={(e)=>setPassword(e.target.value)} />
+        <button type="submit">Log In</button>
+        </form>
+        </>
+    )
+}
+
+export default login
