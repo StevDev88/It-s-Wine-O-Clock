2601da5e06bbe9ef9063a6bc612d8366d6c76d7e
Steven <s.gonza.gills@gmail.com>
2023-06-03 16:01:17 -0400

    New edit.

--- pages/_app.js
+++ pages/_app.js
@@ -1,42 +1,93 @@
 import Layout from "@/components/Layout"
 import { SessionProvider } from "next-auth/react"
 
-import React from 'react';
-import PropTypes from 'prop-types';
+// Version 1, working setup + MUI CSS Baseline added
+
+// import CssBaseline from '@mui/material/CssBaseline';
+
+
+// export default function App({
+//   Component, pageProps: { session, ...pageProps }
+// }) {
+//   return (
+//     <SessionProvider session={session}>
+//         <CssBaseline />
+//         <Layout>
+//             <Component {...pageProps}/>
+//         </Layout>
+//     </SessionProvider>
+//   )
+// }
+
+
+// Version 2, with outdated MUI boilerplate
+
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import Head from 'next/head';
+// import { ThemeProvider } from '@mui/material/styles';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import theme from '../styles/themes/theme';
+
+// export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+
+//   React.useEffect(() => {
+//     const jssStyles = document.querySelector('#jss-server-side');
+//     if (jssStyles) {
+//       jssStyles.parentElement.removeChild(jssStyles);
+//     }
+//   }, []);
+
+//   return (
+//     <React.Fragment>
+//         <Head>
+//             <title>My page</title>
+//             <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+//         </Head>
+//     <ThemeProvider theme={theme}>
+//         <CssBaseline />
+//         <SessionProvider session={session}>
+//         <Layout>
+//             <Component {...pageProps}/>
+//         </Layout>
+//         </SessionProvider>
+//     </ThemeProvider>
+//     </React.Fragment>
+//   );
+// }
+
+// MyApp.propTypes = {
+//   Component: PropTypes.elementType.isRequired,
+//   pageProps: PropTypes.object.isRequired,
+// };
+
+
+import * as React from 'react';
 import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import theme from '../theme';
+import { CacheProvider } from '@emotion/react';
+import theme from '../config/theme';
+import createEmotionCache from '../config/createEmotionCache';
 
-export default function MyApp(props) {
-  const { Component, pageProps } = props;
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
-  React.useEffect(() => {
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
 
+export default function MyApp({ Component, emotionCache = clientSideEmotionCache, pageProps: { session, ...pageProps } }) {
   return (
-    <React.Fragment>
-      <Head>
-        <title>My page</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
+    <CacheProvider value={emotionCache}>
+        <Head>
+            <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
         <SessionProvider session={session}>
-        <Layout>
-            <Component {...pageProps}/>
-        </Layout>
-    </SessionProvider>
-      </ThemeProvider>
-    </React.Fragment>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                    <Layout>
+                        <Component {...pageProps}/>
+                    </Layout>
+            </ThemeProvider>
+        </SessionProvider>
+    </CacheProvider>
   );
 }
-
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
--- pages/_document.js
+++ pages/_document.js
@@ -1,43 +1,61 @@
-import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheets } from '@mui/material/styles';
-import theme from '../theme';
+import { Html, Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          {/* PWA primary color */}
-          <meta name="theme-color" content={theme.palette.primary.main} />
-          <meta name="viewport" content="initial-scale=1, width=device-width" />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+
+    <Head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+    </Head>
+    
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
 
-MyDocument.getInitialProps = async (ctx) => {
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
 
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-    });
+// import React from 'react';
+// import Document, { Html, Head, Main, NextScript } from 'next/document';
+// import { ServerStyleSheets } from '@mui/styles';
+// import theme from '../styles/themes/theme';
 
-  const initialProps = await Document.getInitialProps(ctx);
+// export default class MyDocument extends Document {
+//   render() {
+//     return (
+//       <Html lang="en">
+//         <Head>
+//           {/* PWA primary color */}
+//           <meta name="theme-color" content={theme.palette.primary.main} />
+//           <link
+//             rel="stylesheet"
+//             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+//           />
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// }
 
-  return {
-    ...initialProps,
-    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
-  };
-};
+// MyDocument.getInitialProps = async (ctx) => {
+//   const sheets = new ServerStyleSheets();
+//   const originalRenderPage = ctx.renderPage;
+
+//   ctx.renderPage = () =>
+//     originalRenderPage({
+//       enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+//     });
+
+//   const initialProps = await Document.getInitialProps(ctx);
+
+//   return {
+//     ...initialProps,
+//     styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+//   };
+// };
